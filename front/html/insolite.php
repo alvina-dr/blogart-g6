53ece0470d55f050df7f52af67dde79cc7ed3c44
maurinceros <maurine.cros@gmail.com>
2021-02-11 09:53:21 +0100

    base maurine ^^

---
+++ front/html/insolite.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <HEAD>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/ico" href="/icons/logo.ico"/>
+        <title>B.Game | L'insolite !</title>
+
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <!--linker stylesheet-->
+        <link rel="stylesheet" type="text/css" href="/insolite.css">
+        <link rel="stylesheet" type="text/css" href="/font.css">
+    </HEAD>
+
+    <body>
+        <div>
+            <div class="h22 right container">
+                <img class="gamer" src="/images/gamer.png" alt=""Photo de gamer>
+                <div class="text2">
+                    <img class="insolite" src="/images/insolite.png" alt=""Insolite !>
+                    <p>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                        Vous aimez les jeux vidéos, vous rêvez de faire carrière dans l’e-sport comme vos joueurs préférés. Gotaga, Zywoo, KennyS ? Tous mènent une vie de rêve, entre tournois et partenariats.<br>
+                        Cependant tout ce qui a du succès attire très vite les arnaques… Des écoles d’E-Sport sont apparues ces dernières années en promettant gloire et fantasmes.
+                        Cependant, même si la forme paraît qualitative, le fond de ces études laisse à désirer…
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="h22">
+        <h2>Master ou Mastère ?</h2>
+        <img class="soulignage" src="icons/soulignage.svg">
+        </div>
+        <p>
+            On voit l’émergence d’écoles supérieures privées moyennant un financement contre un diplôme promettant une carrière dans l’e-sport. Par exemple l’EGS (Education Gaming School), située à Bordeaux.
+            <br>
+            <br>
+            Cette école propose une formation en 5 ans afin d’obtenir deux diplômes. Au bout de 3 ans on obtient un “bachelor” et à la fin de la 5ème année un “mastère”.<br>
+            Le problème est que ces diplômes ne sont reconnus ni par l'État Français, ni par aucun autre pays.
+           Ainsi, vos 5 ans dans cette école ne vaudront rien légalement.	Par ailleurs, l’EGS se vante d’avoir un parcours “original” avec leurs filières dans le digital, la communication et l’informatique.
+        </p>
+        <div class="graduation h22"></div>>
+        <p>
+            Cependant, il existe des formations équivalentes, publiques et gratuites. En l’occurrence le DUT MMI (Métiers du Multimédia et de l’Internet), le DUT Information & Communication, ainsi que le DUT Informatique.<br>
+            Ces dernières sont publiques et reconnues par l'État : elles garantissent donc un avenir plus stable aux étudiants qui y sont inscrits.<br>
+            <br>
+            De plus, ces formations existent depuis de nombreuses années, ce qui prouve l’authenticité du diplôme. L’EGS en revanche n’est apparue qu’en 2020.
+            On ne peut donc pas vérifier l’efficacité de ces études, ni si ce diplôme aura une quelconque utilité dans 10 ans.
+        </p>
+        <div class="h22 right">
+            <h2>L'argent ne fait pas le skill</h2>
+            <img class="soulignage2" src="/icons/soulignage_r.svg">
+        </div>
+        <div class="flex">
+            <img class="money" src="/icons/money.svg">
+            <p class="right">
+                Comme dit ci-dessus, les formations équivalentes sont nombreuses et bien plus accessibles financièrement. Ce n’est pas le cas de l’EGS.<br>
+                <br>
+                En effet, l’école privée est entre 7300 et 8000e l’année, soit 38 400€ au total. Si l’on ajoute le prix d’un loyer, les factures, les courses et autres besoins nécessaires (transports, loisirs…), on monte à un tarif qui n’est pas abordable pour un étudiant fraîchement diplômé de son BAC. Il faut donc faire un prêt à la banque, en imaginant que celle-ci accepte.<br>
+                Outre toutes les démarches administratives, cela signifie pour l’étudiant qu’il s’endette pour un prêt de 40 000€ dès 18 ans.
+            </p>
+        </div>
+        <p class="h22">
+            En revanche, les DUT équivalents en écoles publiques sont des formations gratuites si l’élève est éligible à la bourse. Sinon, la formation est à 170e à l’année, ce qui reste tout de même abordable en comparaison.<br>
+	        La formation pourrait justifier son prix par le coût du matériel qu’elle prête, si seulement l’EGS était crédible à ce niveau. Cependant, les “partenaires” qu’elle se vante d’avoir ne justifient pas le prix de la formation. Ils sont de mauvaise qualité et ont mauvaise réputation dans le milieu du gaming.<br>
+            <br>
+            Quitte à mettre de l’argent de côté, autant s’acheter du matériel qualitatif chez soi et par la même occasion s’inscrire dans une école publique.
+        </p>
+        <div>
+            <h2>Les débouchés sont bouchées</h2>
+             <img class="soulignage3" src="/icons/soulignage.svg"> 
+        </div>
+        <div class="container h22">
+            <img class="twitch" src="/images/twitch.png">
+            <div class="text">
+                <p>
+                    Qu’en est-il des débouchés alors ?<br>
+                    Si seulement la promesse d’être un pro gamer s’avérait vraie. Les élèves s’inscrivant dans ces établissements sont sûrs de connaître la célébrité. Néanmoins, on omet souvent de mentionner que peu de gamers gagnent leur vie grâce à leur passion.<br>
+                    <br>
+                    Par ailleurs, le jeu seul ne fait pas toute la carrière.<br>
+                    En effet, les streamers pro doivent aussi alimenter une communauté, et savoir gérer leur réputation. Le problème est que la partie communication et gestion des réseaux sociaux ne se fait qu’en mastère, soit à la fin du diplôme. Et seulement si on choisit la spécialité “communication”. Comment peut-on manager sa présence en ligne et ainsi devenir gamer professionnel reconnu si l’on a pas de cours de communication ?
+                </p>
+            </div>
+        </div>
+        <p>
+            Par ailleurs, le site de l’EGS ne fournit pas de chiffres sur le nombre de pro gamers qui réussissent après leur passage dans l’école. Enfin, aucun coach ni professeurs n’a de palmarès bien impressionnant, de même pour les élèves.<br>
+            Les équipes de l’EGS sont répertoriées sur un site externe, avec leurs matchs et leurs résultats. 0-1 la plupart du temps, ou bien ils finissent 7ème sur une ligue de 8 équipes. Mais l’école se cache bien de garder cette dernière information.
+        </p>
+    </body>
+</html>

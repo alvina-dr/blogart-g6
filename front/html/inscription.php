ae05ec37cea89ae0e18510404c9cf6b530c465cb
maurinceros <maurine.cros@gmail.com>
2021-02-12 10:34:43 +0100

    page inscription début

---
+++ front/html/inscription.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <HEAD>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/ico" href="../assets/icons/logo.ico"/>
+        <title>B.GAME | Inscription</title>
+
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <!--linker stylesheet-->
+        <link rel="stylesheet" type="text/css" href="../css/base.css">
+        <link rel="stylesheet" type="text/css" href="../css/font.css">
+        <link rel="stylesheet" type="text/css" href="../css/insolite.css">
+        <link rel="stylesheet" type="text/css" href="../css/index.css">
+        <link rel="stylesheet" type="text/css" href="../css/footer.css">
+        <link rel="stylesheet" type="text/css" href="../css/inscription.css">
+    </HEAD>
+
+    <body>
+        <div>
+            <img src="../assets/icons/contact.svg">
+        </div>
+    </body>
+</html>

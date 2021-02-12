837008aa286eb94902b77333325a78ee046ab075
maurinceros <maurine.cros@gmail.com>
2021-02-12 11:43:26 +0100

    connexion finie

---
+++ front/html/connexion.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <HEAD>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/ico" href="../assets/icons/logo.ico"/>
+        <title>B.GAME | Connexion</title>
+
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <!--linker stylesheet-->
+        <link rel="stylesheet" type="text/css" href="../css/base.css">
+        <link rel="stylesheet" type="text/css" href="../css/font.css">
+        <link rel="stylesheet" type="text/css" href="../css/insolite.css">
+        <link rel="stylesheet" type="text/css" href="../css/index.css">
+        <link rel="stylesheet" type="text/css" href="../css/footer.css">
+        <link rel="stylesheet" type="text/css" href="../css/inscription.css">
+    </HEAD>
+    <body>
+        <div class="flex">
+            <img src="../assets/icons/hello.svg">
+            <div class="">
+                <img class="h22" src="../assets/icons/terevoila.png">
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+
+                <div class=h22>
+                    <p>Pseudo</p>
+                    <img src="../assets/icons/soulignagedroit.svg">
+                </div>
+                <div class=h22>
+                    <br>
+                    <br>
+                    <p>Mot de passe</p>
+                    <img src="../assets/icons/soulignagedroit.svg">
+                </div>
+                <br>
+                <br>
+                <a><img class="h22 hwg" src="../assets/icons/hwg.png"></a>
+            </div>
+        </div>
+    </body>
+</html>

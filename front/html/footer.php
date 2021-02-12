8f653ba11ca139c3ad1174df76166ce1dc3aa175
maurinceros <maurine.cros@gmail.com>
2021-02-12 10:00:27 +0100

    footer par maurine

---
+++ front/html/footer.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+    <HEAD>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/ico" href="../assets/icons/logo.ico"/>
+        <title>footer</title>
+
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <!--linker stylesheet-->
+        <link rel="stylesheet" type="text/css" href="../css/base.css">
+        <link rel="stylesheet" type="text/css" href="../css/font.css">
+        <link rel="stylesheet" type="text/css" href="../css/insolite.css">
+        <link rel="stylesheet" type="text/css" href="../css/index.css">
+        <link rel="stylesheet" type="text/css" href="../css/footer.css">
+
+        
+    </HEAD>
+    <body>
+        <footer class="h22">
+            <img class="arrow" src="../assets/icons/arrow.svg">
+            <div class="flex h22">
+                <div class="center_t">
+                    <p>Qui sommes nous ?</p>
+                    <br>
+                    <br>
+                    <img class="jp_logo" src="../assets/icons/jp.svg">
+                </div>
+                <div class="center_t">
+                    <p>Tu veux aller où ?</p>
+                    <br>
+                    <br>
+                    <p>Actu</p>
+                    <br>
+                    <p>Acteur clef</p>
+                    <br>
+                    <p>L'insolite !</p>
+                </div>
+                <div class="center_t">
+                    <p>On étais obligés</p>
+                    <br>
+                    <br>
+                    <p>Mentions légales</p>
+                    <br>
+                    <p>C.G.U</p>
+                </div>
+            </div>
+        </footer>
+    </body>
+</html>

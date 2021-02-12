efc0d2407f9e3aee24a325b5fcbf397d41f89685
Astialas <cyrilbas40@gmail.com>
2021-02-12 17:00:11 +0100

    cookies

---
+++ front/html/cookies.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/ico" href="/images/icons/favicon.ico" />
+
+    <!--linker stylesheet-->
+    <link type="text/css" rel="stylesheet" href="../css/cookies.css">
+    <link type="text/css" rel="stylesheet" href="../css/base.css">
+    <link type="text/css" rel="stylesheet" href="../css/font.css">
+    
+</head>
+
+<body>
+<div id="d1" class="bordure_cookies" >
+    <div class="bordure2_cookies">
+        <h4>Ce site web utilise<br> des cookies</h4>
+    </div>
+    <br>
+    <div class="texte_cookies">
+        <p class="pcookies">Nous utilisons des cookies pour vous offrir la meilleure expérience possible. 
+            Cliquez sur accepter pour continuer à magaziner ou pour en savoir plus sur notre politique
+             en matière de cookies ici.</p>
+    </div>
+    <div class="img_cookies">
+        <img src="../assets/images/cookies.png">
+    </div>
+    <div id="togg2" class="croix_cookies">
+        <img src="../assets/images/croix.png">
+    </div>
+    <button id="togg1" class="valider_cookies">
+        <h2 class="cookie_blanc">Accepter</h2>
+    </div>
+</div>
+<script type="text/javascript" src="../js/cookies.js"></script>
---
+++ front/js/cookies.js
@@ -0,0 +1,19 @@
+let togg1 = document.getElementById("togg1");
+let togg2 = document.getElementById("togg2");
+let d1 = document.getElementById("d1");
+
+togg1.addEventListener("click", () => {
+    if(getComputedStyle(d1).display != "none"){
+      d1.style.display = "none";
+    } else {
+      d1.style.display = "block";
+    }
+  })
+
+  togg2.addEventListener("click", () => {
+    if(getComputedStyle(d1).display != "none"){
+      d1.style.display = "none";
+    } else {
+      d1.style.display = "block";
+    }
+  })

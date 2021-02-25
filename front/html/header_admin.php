8faf2bc4b01bba844f381f8a2046af96907cbf60
alvina-dr <alvinadr24@gmail.com>
2021-02-25 15:53:01 +0100

    ajout du header dans index1.php pour pouvoir revenir vers le site quand on est admin

---
+++ front/html/header_admin.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="content" content="Le blog du jeu vidéo bordelais.">
+	<title>Le header</title>
+	
+	<link rel="icon" type="image/ico" href="../assets/icons/logo.ico" />
+
+	<!--linker stylesheet-->
+	<link rel="stylesheet" type="text/css" href="../css/base.css">
+	<link rel="stylesheet" href="../css/font.css">
+	<link rel="stylesheet" href="../css/header.css">
+	<link rel="stylesheet" href="../css/footer.css">
+	<link rel="stylesheet" href="../css/insolite.css">
+
+</head>
+
+<body>
+	<header>
+		<balise id="hautdepage"></balise>
+		<div id="navbar">
+			<div class="column">
+			<a class="logo_link" href="./index.php"><img class="logo" src="/front/assets/icons/logo bg.svg"></a>
+			</div>
+			<ul class="menu_list">
+				<li><a href="./hacktu.php">Actu</a></li>
+				<li><a href="./actors.php">Acteur clef</a></li>
+				<li><a href="./insolite.php">Insolite</a></li>
+				<li><a href="../../index1.php">Admin</a></li>
+			</ul>
+			<ul class="log">
+				<li><a href="./inscription.php">Inscription</a></li>
+				<li ><a class="connexion" href="./connexion.php"></a></li>
+			</ul>
+			</div>
+	</header>
+</body>
+</html>

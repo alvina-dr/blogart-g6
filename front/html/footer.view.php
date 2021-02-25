ac9ca85663c1a38f8f95fc090af7e9948e20f85e
alvina-dr <alvinadr24@gmail.com>
2021-02-25 16:30:09 +0100

    relinkage des cookies

--- front/html/footer.view.php
+++ front/html/footer.view.php
@@ -7,9 +7,9 @@
     <link rel="icon" type="image/ico" href="/images/icons/favicon.ico" />
 
     <!--linker stylesheet-->
-    <link type="text/css" rel="stylesheet" href="../css/cookies.css">
-    <link type="text/css" rel="stylesheet" href="../css/base.css">
-    <link type="text/css" rel="stylesheet" href="../css/font.css">
+    <link type="text/css" rel="stylesheet" href="../../front/css/cookies.css">
+    <link type="text/css" rel="stylesheet" href="../..front/css/base.css">
+    <link type="text/css" rel="stylesheet" href="../../front/css/font.css">
     
 </head>
 
@@ -23,18 +23,18 @@
     <div class="texte_cookies">
         <p class="pcookies">Nous utilisons des cookies pour vous offrir la meilleure expérience possible. 
             Cliquez sur accepter pour continuer à naviguer ou pour en savoir plus sur notre politique
-             en matière de cookies <a class="ici" href="mentionslégales.php">ici</a>.  </p>
+             en matière de cookies <a class="ici" href="../../front/html/mentionslégales.php">ici</a>.  </p>
     </div>
     <div class="img_cookies">
-        <img src="../assets/images/cookies.png">
+        <img src="../../front/assets/images/cookies.png">
     </div>
     <div id="togg2" class="croix_cookies">
-        <img src="../assets/images/croix.png">
+        <img src="../../front/assets/images/croix.png">
     </div>
     <button class="valider_cookies">
-        <h2 class="cookie_blanc"><a href="accept_cookie.php">Accepter</a></h2>
+        <h2 class="cookie_blanc"><a href="../../front/html/accept_cookie.php">Accepter</a></h2>
     <button id="togg3" class="refuser_cookies">
         <h2 class="cookie_blanc2">Refuser</h2>
 </div>
-<script type="text/javascript" src="../js/cookies.js"></script>
+<script type="text/javascript" src="../../front/js/cookies.js"></script>
 <?php } ?>
